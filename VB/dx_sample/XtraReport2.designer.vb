32cae6dede73aced18b861351b268413d7215289
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/dx_sample/XtraReport2.designer.vb
@@ -0,0 +1,144 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace dx_sample
+	Partial Public Class XtraReport2
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.xrLabel3 = New DevExpress.XtraReports.UI.XRLabel()
+			Me.xrLabel2 = New DevExpress.XtraReports.UI.XRLabel()
+			Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
+			Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
+			Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
+            Me.nwindDataSet1 = New nwindDataSet()
+            Me.customersTableAdapter = New nwindDataSetTableAdapters.CustomersTableAdapter()
+            Me.ordersTableAdapter = New nwindDataSetTableAdapters.OrdersTableAdapter()
+			Me.customerID = New DevExpress.XtraReports.Parameters.Parameter()
+			CType(Me.nwindDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel3, Me.xrLabel2, Me.xrLabel1})
+			Me.Detail.HeightF = 20.91665F
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' xrLabel3
+			' 
+			Me.xrLabel3.Borders = DevExpress.XtraPrinting.BorderSide.Bottom
+			Me.xrLabel3.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Orders.ShipName")})
+			Me.xrLabel3.LocationFloat = New DevExpress.Utils.PointFloat(200F, 0F)
+			Me.xrLabel3.Name = "xrLabel3"
+			Me.xrLabel3.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrLabel3.SizeF = New System.Drawing.SizeF(252.0834F, 20.91665F)
+			Me.xrLabel3.StylePriority.UseBackColor = False
+			Me.xrLabel3.StylePriority.UseBorders = False
+			Me.xrLabel3.Text = "xrLabel3"
+			' 
+			' xrLabel2
+			' 
+			Me.xrLabel2.Borders = DevExpress.XtraPrinting.BorderSide.Bottom
+			Me.xrLabel2.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Orders.CustomerID")})
+			Me.xrLabel2.LocationFloat = New DevExpress.Utils.PointFloat(100F, 0F)
+			Me.xrLabel2.Name = "xrLabel2"
+			Me.xrLabel2.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrLabel2.SizeF = New System.Drawing.SizeF(100F, 20.91665F)
+			Me.xrLabel2.StylePriority.UseBackColor = False
+			Me.xrLabel2.StylePriority.UseBorders = False
+			Me.xrLabel2.Text = "xrLabel2"
+			' 
+			' xrLabel1
+			' 
+			Me.xrLabel1.Borders = DevExpress.XtraPrinting.BorderSide.Bottom
+			Me.xrLabel1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Orders.OrderID", "{0:Order \##}"), New DevExpress.XtraReports.UI.XRBinding("Tag", Nothing, "Orders.CustomerID")})
+			Me.xrLabel1.LocationFloat = New DevExpress.Utils.PointFloat(0F, 0F)
+			Me.xrLabel1.Name = "xrLabel1"
+			Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrLabel1.SizeF = New System.Drawing.SizeF(100F, 20.91665F)
+			Me.xrLabel1.StylePriority.UseBackColor = False
+			Me.xrLabel1.StylePriority.UseBorders = False
+			Me.xrLabel1.Text = "xrLabel1"
+			' 
+			' TopMargin
+			' 
+			Me.TopMargin.Name = "TopMargin"
+			Me.TopMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' BottomMargin
+			' 
+			Me.BottomMargin.Name = "BottomMargin"
+			Me.BottomMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' nwindDataSet1
+			' 
+			Me.nwindDataSet1.DataSetName = "nwindDataSet"
+			Me.nwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+			' 
+			' customersTableAdapter
+			' 
+			Me.customersTableAdapter.ClearBeforeFill = True
+			' 
+			' ordersTableAdapter
+			' 
+			Me.ordersTableAdapter.ClearBeforeFill = True
+			' 
+			' customerID
+			' 
+			Me.customerID.Description = "Parameter1"
+			Me.customerID.Name = "customerID"
+			Me.customerID.Value = ""
+			Me.customerID.Visible = False
+			' 
+			' XtraReport2
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.TopMargin, Me.BottomMargin})
+			Me.DataAdapter = Me.ordersTableAdapter
+			Me.DataMember = "Orders"
+			Me.DataSource = Me.nwindDataSet1
+			Me.FilterString = "[CustomerID] = ?customerID"
+			Me.Parameters.AddRange(New DevExpress.XtraReports.Parameters.Parameter() { Me.customerID})
+			Me.Version = "12.1"
+			CType(Me.nwindDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private TopMargin As DevExpress.XtraReports.UI.TopMarginBand
+		Private BottomMargin As DevExpress.XtraReports.UI.BottomMarginBand
+		Private xrLabel3 As DevExpress.XtraReports.UI.XRLabel
+		Private xrLabel2 As DevExpress.XtraReports.UI.XRLabel
+		Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+		Private nwindDataSet1 As nwindDataSet
+		Private customersTableAdapter As nwindDataSetTableAdapters.CustomersTableAdapter
+		Private ordersTableAdapter As nwindDataSetTableAdapters.OrdersTableAdapter
+		Private customerID As DevExpress.XtraReports.Parameters.Parameter
+	End Class
+End Namespace
